686449d6ecb2c4d8af0e261a43ee9ffc5ad62195
EonGaminq <eonkid9000@gmail.com>
2023-05-20 03:13:30 +0530

    Added checks to see if player is already logged in. if !Logged in, Open PIN GUI. Added a check to see if player has registered, if !Registered, execute /register + pin command, else execute /login + pin command as Player.

--- src/main/java/com/xauth/xAuth.java
+++ src/main/java/com/xauth/xAuth.java
@@ -2,9 +2,7 @@ package com.xauth;
 
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
-import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
-import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -13,10 +11,14 @@ import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.event.inventory.InventoryType;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.scheduler.BukkitScheduler;
+
 
 import java.util.ArrayList;
 import java.util.List;
 
+import fr.xephi.authme.api.v3.AuthMeApi;
 public class xAuth extends JavaPlugin implements CommandExecutor, Listener {
 
     private List<Integer> clickedSlots;
@@ -34,20 +36,8 @@ public class xAuth extends JavaPlugin implements CommandExecutor, Listener {
         getLogger().info("xAuth plugin has been disabled.");
     }
 
-    @Override
-    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        if (command.getName().equalsIgnoreCase("auth")) {
-            if (sender instanceof Player) {
-                Player player = (Player) sender;
 
-                // Delay opening the dispenser GUI for 1 tick to ensure it is properly rendered
-                Bukkit.getScheduler().runTaskLater(this, () -> openDispenserGUI(player), 1);
-            }
-            return true;
-        }
-        return false;
-    }
-
+    //PIN GUI inventory Creation
     private void openDispenserGUI(Player player) {
         Inventory dispenserGUI = Bukkit.createInventory(null, InventoryType.DISPENSER, "PIN GUI");
 
@@ -60,6 +50,26 @@ public class xAuth extends JavaPlugin implements CommandExecutor, Listener {
         player.openInventory(dispenserGUI);
     }
 
+    //Check to see if the player is logged in, If not, open login GUI. The task has 6 ticks of delay to let authMe have priority.
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        Player player = event.getPlayer();
+        BukkitScheduler scheduler = Bukkit.getServer().getScheduler();
+        scheduler.runTaskLater(this, () -> {
+        if (AuthMeApi.getInstance().isAuthenticated(player)) {
+            // Player is already logged in
+            player.sendMessage("[xAuth] You are already logged in, Skipping Login GUI");
+        } else {
+            // Player is not logged in
+            player.sendMessage("[xAuth] You are not logged in. Login using GUI");
+            openDispenserGUI(player);
+        }
+
+        }, 6);
+
+    }
+
+    //If Login GUI is open, Start recording PIN into an array
     @EventHandler
     public void onInventoryClick(InventoryClickEvent event) {
         if (event.getView().getTitle().equals("PIN GUI")) {
@@ -71,13 +81,23 @@ public class xAuth extends JavaPlugin implements CommandExecutor, Listener {
                 clickedSlots.add(slot);
                 player.sendMessage("Clicked on slot: " + slot);
 
+                // Start combining all the numbers into a string
                 if (clickedSlots.size() >= 4) {
-                    StringBuilder pinBuilder = new StringBuilder("Your PIN is ");
+                    StringBuilder pinBuilder = new StringBuilder("");
                     for (int i = 0; i < 4; i++) {
                         pinBuilder.append(clickedSlots.get(i));
                     }
-                    player.sendMessage(pinBuilder.toString());
+                    String pin = pinBuilder.toString();
+                    player.sendMessage("Your Pin is " + pin); // Send PIN Message
                     clickedSlots.clear();
+                    player.closeInventory(); //closeInventory once 4 digit PIN has been typed out.
+
+                    //If player is already registered, use /login command, if not, use /register command
+                    if (AuthMeApi.getInstance().isRegistered(player.getName())) {
+                        Bukkit.dispatchCommand(player, "login " + pin);
+                    } else {
+                        Bukkit.dispatchCommand(player, "register " + pin + " " + pin);
+                    }
                 }
             }
         }
